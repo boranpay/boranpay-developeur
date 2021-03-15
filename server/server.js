4e3d15e26cc4b607c8d57079c7dac1bc79857f99
Boclair Temgoua <temgoua2000@gmail.com>
2021-03-15 01:04:11 +0100

    fix client

--- server/server.js
+++ server/server.js
@@ -6,23 +6,7 @@ const PORT = process.env.PORT || '8080';
 
 const server = http.createServer(app)
 
-if (process.env.NODE_ENV === "development") {
-  server.listen(PORT, () => {
-    console.log(`Local: ${process.env.NODE_APP_URL}:${PORT}`);
-    console.log('*************************************\n');
-  })
-} else {
-  // Sync sequelize models then start Express app
-  // =============================================
-  db.sequelize.sync({ force: false, alter: true })
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(`Local: ${process.env.NODE_APP_URL}:${PORT}`);
-        console.log('*************************************\n');
-      });
-    }) 
-    .then(() => {
-      console.log('\n*************************************');
-      console.log(`${process.env.DB_USERNAME} database connected | database name: ${process.env.DB_DATABASE}`);
-    });
-}
+server.listen(PORT, () => {
+  console.log(`Local: ${process.env.NODE_APP_URL}:${PORT}`);
+  console.log('*************************************\n');
+})
